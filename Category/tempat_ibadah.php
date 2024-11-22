601dbf6e9e3e7ec7d9555c7651ae9d3c9f6c666c
Aditya Ryan Wardana <adityaryanwardana@gmail.com>
2024-11-22 19:44:15 +0700

    tempat_ibadah.php

---
+++ Category/tempat_ibadah.php
@@ -0,0 +1,101 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tempat Ibadah</title>
+    <link rel="stylesheet" href="tempat_ibadah_style.css">
+</head>
+<body>
+    <div class="header">
+        <img alt="Serenity Trails Logo" height="50" src="serenity.png" width="100"/>
+            <nav>
+                <a href="home.html">Home</a>
+                <a href="Kategori.html">Category</a>
+                <a href="destinasi.html">Destination</a>
+                <a href="informasi.html">Information</a>
+            </nav>
+        <div class="user-icon">
+            <i class="fas fa-user-circle"></i>
+            <img alt="user-icon" src="image.jpeg">
+            <a href="login.html"></a>
+        </div>
+    </div>
+    <div class="content">
+        <h1>ISLAM</h1>
+        <div class="cards">
+            <div class="card">
+                <img alt="Masjid Sultan Mahmud Riayat Syah" height="150" src="masjid_sultan.jpeg" width="250"/>
+                <a href="masjid_sultan.html"><h2>Masjid Sultan Mahmud Riayat Syah</h2></a>
+                <p>Masjid Sultan Mahmud Riayat Syah adalah permata baru Kota Batam yang wajib Anda kunjungi. Dengan arsitektur megah dan kapasitas yang besar, masjid ini menjadi pusat ibadah sekaligus destinasi wisata religi yang menarik. Nikmati keindahan arsitektur Islam modern dan pemandangan kota Batam dari menara pandangnya yang menjulang tinggi.</p>
+            </div>
+            <div class="card">
+                <img alt="Masjid Agung Kota Batam" height="150" src="masjid_agung.jpg" width="250"/>
+                <h2>Masjid Agung Kota Batam</h2>
+                <p>salah satu masjid yang memiliki desain arsitektur yang unik dengan kubah berbentuk limas segi empat atau menyerupai piramida. dan masjid ini terletak di pusat kota batam yaitu di Batam Kota.</p>
+            </div>
+            <div class="card">
+                <img alt="Masjid Tanwirun Naja (Masjid Tanjak)" height="150" src="ibadah.jpg" width="250"/>
+                <h2>Masjid Tanwirun Naja (Masjid Tanjak)</h2>
+                <p>yang lebih dikenal dengan Masjid Tanjak adalah sebuah Masjid yang terinspirasi dari bentuk tanjak, yaitu hiasan kepala khas Melayu yang melambangkan kewibawaan.dan terletak tepat didepan Bandara Hang Nadim Batam.</p>
+            </div>
+            <div class="card">
+                <img alt="Masjid Cheng Hoo Bengkong Laut" height="150" src="masjid_chenghoo.jpg" width="250"/>
+                <h2>Masjid Cheng Hoo Bengkong Laut</h2>
+                <p>sebuah masjid unik di Batam yang memadukan unsur budaya Tionghoa dan Islam. Masjid ini dibangun sebagai bentuk penghormatan kepada Laksamana Cheng Ho,dan masjid ini terletak dikawasan bengkong laut batam.</p>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <h1>KRISTEN</h1>
+        <div class="cards">
+            <div class="card">
+                <img alt="Gereja Immanuel" height="150" src="gereja_immanuel.jpg" width="250"/>
+                <h2>Gereja_Immanuel</h2>
+                <p>salah satu gereja Protestan yang cukup dikenal di Kota Batam. Gereja ini memiliki sejarah yang panjang dan menjadi pusat ibadah serta kegiatan rohani bagi banyak umat Kristiani di Batam.</p>
+            </div>
+            <div class="card">
+                <img alt="Gereja Santo Damian" height="150" src="gereja_santodamian.jpg" width="250"/>
+                <h2>Gereja Paroki Santo Damian</h2>
+                <p>salah satu gereja Katolik yang cukup besar dan aktif di wilayah Batam. Gereja ini berada di bawah naungan Keuskupan Pangkal Pinang dan memiliki peran penting dalam kehidupan umat Katolik di sekitar wilayah Bengkong, Nongsa, dan Batam Center.</p>
+            </div>
+            <div class="card">
+                <img alt="HKBP Batam Center" height="150" src="gereja_hkbp_batamcenter.png" width="250"/>
+                <h2>HKBP Batam Center</h2>
+                <p>salah satu jemaat dari Huria Kristen Batak Protestan (HKBP) yang terletak di kota Batam. Gereja ini menjadi pusat ibadah dan kegiatan rohani bagi umat Kristiani, khususnya warga Batak yang berada di sekitar wilayah Batam Centre.</p>
+            </div>
+            <div class="card">
+                <img alt="Gereja Paroki Santo Petrus" height="150" src="gereja_santopetrus.jpg" width="250"/>
+                <h2>Gereja Paroki Santo Petrus</h2>
+                <p>sebuah gereja Katolik yang umumnya memiliki peran penting dalam kehidupan spiritual umat Katolik di wilayahnya. Nama "Santo Petrus" diambil dari salah satu rasul Yesus Kristus yang dianggap sebagai "batu karang" bagi Gereja Katolik.</p>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <h1>BUDHA</h1>
+        <div class="cards">
+            <div class="card">
+                <img alt="Maha Vihara Duta Maitreya" height="150" src="maha_vihara_duta.JPG" width="250"/>
+                <h2>Maha Vihara Duta Maitreya</h2>
+                <p>sebuah vihara Buddha yang sangat terkenal dan menjadi salah satu landmark agama Buddha terbesar di Asia Tenggara, khususnya di Indonesia. Terletak di Kota Batam, vihara ini tidak hanya menjadi tempat ibadah bagi umat Buddha, tetapi juga menjadi destinasi wisata yang menarik bagi pengunjung dari berbagai latar belakang.</p>
+            </div>
+            <div class="card">
+                <img alt="Vihara Budhi Bhakti" height="150" src="vihara budhi bhakti.jpg" width="250"/>
+                <h2>Vihara Budhi Bhakti</h2>
+                <p> salah satu vihara Buddha tertua dan paling terkenal di Kota Batam. Vihara ini tidak hanya menjadi tempat ibadah bagi umat Buddha, tetapi juga menjadi bagian penting dari sejarah dan budaya kota Batam.</p>
+            </div>
+            <div class="card">
+                <img alt="Maitri Vihara Sagara" height="150" src="maitri vihara sagara.jpeg" width="250"/>
+                <h2>Maitri Vihara Sagara</h2>
+                <p>salah satu vihara Buddha yang terletak di Batam, Kepulauan Riau. Meskipun namanya mungkin tidak sebesar vihara-vihara lain yang lebih terkenal, Vihara Maitri Sagara memiliki peran penting dalam kehidupan umat Buddha di sekitar wilayahnya.</p>
+            </div>
+            <div class="card">
+                <img alt="Vihara Samudra Dharma" height="150" src="vihara samudra dharma.jpg" width="250"/>
+                <h2>Vihara Samudra Dharma</h2>
+                <p>salah satu tempat ibadah umat Buddha yang cukup terkenal di Kota Batam. Awalnya, pada tahun 1990, vihara ini hanyalah sebuah gubuk kecil yang sederhana. Namun, seiring berjalannya waktu dan berkat dukungan dari umat, Vihara Samudra Dharma kini telah tumbuh menjadi kompleks yang megah dan indah.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
