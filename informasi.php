64a9629b4ea92dbe3f5a88eb383e5ec1e996a710
Victrysius <vinoallvarnokayan@gmail.com>
2024-11-23 20:05:40 +0700

    revisi

--- informasi.php
+++ informasi.php
@@ -8,17 +8,23 @@
     <title>Serenity Trails</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&amp;family=Great+Vibes&amp;display=swap"
-        rel="stylesheet" /></head>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&amp;family=Great+Vibes&amp;display=swap"rel="stylesheet" /></head>
+    <link rel="stylesheet" href="css/kategori_style.css">
 
 <body>
   <div class="header">
-    <?php
-
-        include ('header.php')
-
-    ?>
+    <img alt="Serenity Trails Logo" height="50" src="serenity.jpeg" width="100"/>
+        <nav>
+            <a href="landing page.php">Home</a>
+            <a href="kategori.php">Category</a>
+            <a href="destinasi.php">Destination</a>
+            <a href="informasi.php">Information</a>
+        </nav>
+    <div class="user-icon">
+        <i class="fas fa-user-circle"></i>
+        <a href="Menu_Login.php"><img alt="user-icon" src="image.jpeg">
     </div>
+</div>
 
     <div class="content"> 
         <div class="column">
@@ -35,6 +41,6 @@
             
         </div>
     </div>
-
+    
  </body>
 </html>
