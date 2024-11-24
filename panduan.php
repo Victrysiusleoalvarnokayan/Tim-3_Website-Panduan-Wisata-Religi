e7f9ae741f01073ae734997c7e6bdd6b8bbd07ad
Omegaclarizamanuela <megaclrsa@gmail.com>
2024-11-24 22:50:03 +0700

    revisi

--- panduan.php
+++ panduan.php
@@ -4,7 +4,7 @@
 <html>
 <head>
     <title>Panduan Tempat Wisata Religi</title>
-    <link rel="stylesheet" href="css/panduan_style.css"> 
+    <link rel="stylesheet" href="css/kategori_style.css"> 
 </head>
 
 <body>
@@ -18,7 +18,7 @@
             </nav>
         <div class="user-icon">
             <i class="fas fa-user-circle"></i>
-            <img alt="user-icon" src="image.jpeg">
+            <img alt="user-icon" src="img/image.jpeg">
             <a href="login.html"></a>
         </div>
     </div>
@@ -26,7 +26,7 @@
         <h1>Panduan Wisata Religi </h1>
         <div class="cards">
             <div class="card">
-                <img alt="Masjid Sultan Mahmud Riayat Syah" height="150" src="masjid sultan.jpg" width="250"/>
+                <img alt="Masjid Sultan Mahmud Riayat Syah" height="150" src="img/masjid_sultan1.jpeg" width="250"/>
                 <h2>Masjid Sultan Mahmud Riayat Syah</h2>
                 <p>Lokasi: <strong>Tanjung Uncang, Kecamatan Batu Aji, Kota Batam.</p>
                 <p>Diresmikan: <strong>Tahun 2019, masjid ini merupakan yang terbesar di Kepulauan Riau.</p>
@@ -36,7 +36,7 @@
             </div>
         <div class="cards">
             <div class="card">
-                <img alt="Masjid Agung Kota Batam" height="150" src="masjid agung.jpg" width="250"/>
+                <img alt="Masjid Agung Kota Batam" height="150" src="img/masjid_agung1.jpg" width="250"/>
                 <h2>Masjid Agung Kota Batam</h2>
                 <p>Lokasi: <strong>Jl. Engku Putri, Tlk. Tering, Kec. Batam Kota, Kota Batam, Kepulauan Riau.</p>
                 <p>Diresmikan: <strong>Tahun 2005, masjid ini merupakan masjid yang terletak dibatam Kepulauan Riau.</p>
@@ -46,7 +46,7 @@
             </div>
         <div class="cards">
             <div class="card">
-                <img alt="Masjid Tanwirun Naja (Masjid Tanjak)" height="150" src="masjid tanjak.jpg" width="250"/>
+                <img alt="Masjid Tanwirun Naja (Masjid Tanjak)" height="150" src="img/ibadah.jpg" width="250"/>
                 <h2>Masjid Tanwirun Naja (Masjid Tanjak)</h2>
                 <p>Lokasi: <strong>Patung Rajawali,Batu Besar,Kecamatan Nongsa,Batam</p>
                 <p>Diresmikan: <strong>Tahun 2022, masjid ini merupakan tempat yang populer walapun tidak sebesar masjid agung batam.</p>
@@ -57,7 +57,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="Masjid Cheng Hoo Bengkong Laut" height="150" 
-                src="masjid cheng.jpg" width="250"/>
+                src="img/masjid_chenghoo1.jpg" width="250"/>
                 <h2>Masjid Cheng Hoo Bengkong Laut</h2>
                 <p>Lokasi: <strong>Tj. Buntung, Kec. Bengkong, Kota Batam, Kepulauan Riau.</p>
                 <p>Diresmikan: <strong>Tahun 2015, masjid ini adalah salah satu destinasi wisata yang unik karena menggabungkan unsur budaya Tionghoa dan Islam.</p>
@@ -68,7 +68,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="Vihara Budhi Bhakti" height="150" 
-                src="vihara bukti.jpg" width="250"/>
+                src="img/vihara budhi bhakti.jpg" width="250"/>
                 <h2>Vihara Budhi Bhakti</h2>
                 <p>Lokasi: <strong>Jl. Pembangunan, Lubuk Baja Kota, Kec. Lubuk Baja, Kota Batam, Kepulauan Riau.</p>
                 <p>Diresmikan: <strong>Tahun 1986, Vihara ini telah menjadi salah satu tempat ibadah utama bagi umat Buddha di Batam sekaligus destinasi wisata budaya yang populer bagi wisatawan lokal dan internasional.</p>
@@ -79,7 +79,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="Pura Agung Amerta Bhuana" height="150" 
-                src="pura agung.jpg" width="250"/>
+                src="img/pura_agung.jpg" width="250"/>
                 <h2>Pura Agung Amerta Bhuan</h2>
                 <p>Lokasi: <strong>Jl. Gajah Mada No.3, sei Ladi, Kec. Sekupang, Kota Batam, Kepulauan Riau</p>
                 <p>Diresmikan: <strong>Tahun 1999, Pura ini dibangun sebagai tempat ibadah bagi umat Hindu di wilayah Batam dan sekitarnya, serta menjadi simbol keberagaman budaya dan agama di Kepulauan Riau</p>
@@ -90,7 +90,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="Vihara Duta Maitreya Monastery" height="150" 
-                src="vihara.jpg" width="250"/>
+                src="img/maha_vihara_duta.JPG" width="250"/>
                 <h2>Vihara Duta Maitreya Monastery </h2>
                 <p>Lokasi: <strong>Komplek Maha Vihara Duta Maitreya Bukit Beruntung, Sungai Panas, Kec. Batam Kota, Kota Batam, Kepulauan Riau</p>
                 <p>Diresmikan: <strong>Tahun 1991, Vihara ini menjadi salah satu vihara terbesar di Indonesia dan pusat kegiatan keagamaan serta kebudayaan bagi umat Buddha di Batam.</p>
@@ -101,7 +101,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="GPIB Immanuel Batam" height="150" 
-                src="gpib.jpg" width="250"/>
+                src="img/gereja_immanuel.jpg" width="250"/>
                 <h2>GPIB Immanuel Batam </h2>
                 <p>Lokasi: <strong> Jl. Raja Ali H., Kp. Seraya, Kec. Batu Ampar, Kota Batam, Kepulauan Riau</p>
                 <p>Diresmikan: <strong>Tahun 1991, Gereja ini telah menjadi salah satu pusat ibadah utama bagi jemaat Kristen Protestan di Batam dan merupakan bagian penting dari sejarah perkembangan gereja di wilayah tersebut.</p>
@@ -112,7 +112,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="Paroki Santo Damian" height="150" 
-                src="santo damian.jpg" width="250"/>
+                src="img/gereja_santodamian.jpg" width="250"/>
                 <h2>Paroki Santo Damian</h2>
                 <p>Lokasi: <strong>Kompleks Green Town, Jl. Yos Sudarso, Bengkong Laut, Kec. Bengkong, Kota Batam, Kepulauan Riau</p>
                 <p>Diresmikan: <strong>Tahun 1993, Gereja ini adalah tempat yang indah untuk beribadah dan juga sebuah lokasi yang menarik bagi mereka yang ingin memahami lebih dalam tentang komunitas Katolik diBatam.</p>
@@ -123,7 +123,7 @@
         <div class="cards">
             <div class="card">
                 <img alt="Gereja Paroki Santo Petrus" height="150" 
-                src="petrus.jpg" width="250"/>
+                src="img/gereja_santopetrus.jpg" width="250"/>
                 <h2>Gereja Paroki Santo Petrus</h2>
                 <p>Lokasi: <strong>Jl. Anggrek Blok II No. 1, Lubuk Baja Kota, Lubuk Baja, Lubuk Baja Kota, Kec. Lubuk Baja, Kota Batam, Kepulauan Riau</p>
                 <p>Diresmikan: <strong>Tahun 1995, merupakan tempat ibadah yang damai dan indah, serta menyuguhkan pengalaman spiritual yang mengesankan bagi pengunjung.</p>
